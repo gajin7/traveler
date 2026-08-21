d8ac47a5b8502722f1f6a05f7b44de3380f8106c
Jovan Gajin <39369555+gajin7@users.noreply.github.com>
2026-08-21 19:56:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V="cicerone-2026.08.21-1627";
+const V="cicerone-2026.08.21-1657";
 const SHELL=["./","index.html"];
 self.addEventListener("install",e=>{ e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())); });
 self.addEventListener("activate",e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
